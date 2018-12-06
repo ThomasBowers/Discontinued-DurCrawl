dc6fdd92f013e30e9b5142d9982411bd79f721c1
DavidAHazra <david.ogden493@googlemail.com>
2018-12-06 17:15:27 +0000

    Made app work

--- NodeSite/app.js
+++ NodeSite/app.js
@@ -1,14 +1,51 @@
-const http = require('http');
+var http = require('http');
+var fs = require('fs');
 
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1"
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+const server = http.createServer(function(request, response) {  
+		const request_url = request.url
+		var resource_url = "."
+		var content_type = {"Content-Type" : "text/html"}
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+		var extension = request_url.split('.').pop()
+
+		if (request_url == "/") {
+			resource_url += "index.html"
+		} else {
+			resource_url += request_url
+		}
+
+		switch (extension) {
+			case "jpg": {
+				content_type["Content-Type"] = "image/jpg"
+				break
+			}
+
+			case "png": {
+				content_type["Content-Type"] = "image/png"
+				break
+			}
+
+			case "js": {
+				content_type["Content-Type"] = "text/javascript"
+				break
+			}
+
+			case "css": {
+				content_type["Content-Type"] = "text/css"
+				break
+			}
+		}
+
+        response.writeHeader(200, content_type);
+        var contents = fs.readFileSync(resource_url);
+        response.write(contents)
+        response.end(); 
+
+		 
+
+
+        
+    }).listen(port, hostname);
