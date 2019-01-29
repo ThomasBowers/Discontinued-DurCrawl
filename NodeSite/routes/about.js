b20f31938528157e8b46b698e16fb8ee4e8cd11a
thomasbowers <thomas.bowers@durham.ac.uk>
2019-01-29 16:19:52 +0000

    added college info json file started work on database class

--- NodeSite/routes/about.js
+++ NodeSite/routes/about.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+/*College data*/
 
 /* GET about page. */
 router.get('/', function(req, res, next) {
--- NodeSite/routes/colleges.js
+++ NodeSite/routes/colleges.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET college page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('colleges', { title: 'colleges' });
 });
-
 module.exports = router;
