cb9a022d727707ac3e1f66cb4c7538f935bf069b
thomasbowers <thomas.bowers@durham.ac.uk>
2019-01-31 16:30:48 +0000

    Using express properly now

--- NodeSite/routes/colleges.js
+++ NodeSite/routes/colleges.js
@@ -1,8 +1,11 @@
+const file_system = require('fs');
 var express = require('express');
 var router = express.Router();
-
+var allColleges = file_system.readFileSync('./NodeSite/public/colleges.json');
+var json_object = JSON.parse(allColleges);
+console.log(json_object['cuths-brooks']);
 /* GET college page. */
 router.get('/', function(req, res) {
-  res.render('colleges', { title: 'colleges' });
+  res.render('colleges', { title: 'colleges' }, {colleges: json_object});
 });
 module.exports = router;
