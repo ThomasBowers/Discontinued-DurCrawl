bdef964daa993fb1831c04db38cedd2e091b5f51
thomasbowers <thomas.bowers@durham.ac.uk>
2019-01-31 16:39:08 +0000

    reformatted colleges.json

--- NodeSite/crawl.js
+++ NodeSite/crawl.js
@@ -1,6 +1,6 @@
 /*Type is a string containing "bailey", "hill" or "other"*/
 function getByType(type) {
-    var allColleges = require('colleges.json');
+    var allColleges = require('./public/colleges');
     var collegeList = [];
     for (var i = 0; i < allColleges.length; i++) {
         if (allColleges[i][5] === type) {
@@ -11,7 +11,7 @@ function getByType(type) {
 }
 /*Gets college where the string name is the name of the college*/
 function getByName(name) {
-    var allColleges = require('colleges.json');
+    var allColleges = require('./public/colleges');
     for (var i = 0; i < allColleges.length; i++) {
         if (allColleges[i][0] === name) {
             return allColleges[i]
