3e5767f22649b6dea7c90b50c54ed1b1352c8879
thomasbowers <thomas.bowers@durham.ac.uk>
2019-02-02 14:20:04 +0000

    starts at your location and finishes plus map now styled

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -2,33 +2,131 @@ function init_map(newNode, offset) {
     var durham_location = {lat: 54.770764, lng: -1.572726};
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 14,
-        center: durham_location
+        center: durham_location,
+        styles: [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+                featureType: 'administrative.locality',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#d59563'}]
+            },
+            {
+                featureType: 'poi',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#d59563'}]
+            },
+            {
+                featureType: 'poi.park',
+                elementType: 'geometry',
+                stylers: [{color: '#263c3f'}]
+            },
+            {
+                featureType: 'poi.park',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#6b9a76'}]
+            },
+            {
+                featureType: 'road',
+                elementType: 'geometry',
+                stylers: [{color: '#38414e'}]
+            },
+            {
+                featureType: 'road',
+                elementType: 'geometry.stroke',
+                stylers: [{color: '#212a37'}]
+            },
+            {
+                featureType: 'road',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#9ca5b3'}]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'geometry',
+                stylers: [{color: '#746855'}]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'geometry.stroke',
+                stylers: [{color: '#1f2835'}]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#f3d19c'}]
+            },
+            {
+                featureType: 'transit',
+                elementType: 'geometry',
+                stylers: [{color: '#2f3948'}]
+            },
+            {
+                featureType: 'transit.station',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#d59563'}]
+            },
+            {
+                featureType: 'water',
+                elementType: 'geometry',
+                stylers: [{color: '#17263c'}]
+            },
+            {
+                featureType: 'water',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#515c6d'}]
+            },
+            {
+                featureType: 'water',
+                elementType: 'labels.text.stroke',
+                stylers: [{color: '#17263c'}]
+            }
+        ]
     });
-    if (navigator.geolocation && startL) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-            latlong.push({location: new google.maps.LatLng(position.coords.latitude, position.coords.longitude)});
-        }, function () { console.log('gps-unavailable')
-        });
-    }
-    var mapPlot = [];
     var latlong = [];
-    for (var j = 0; j < collegeWP.length; j++) {
-        if (routeNeeded) {
-            latlong.push({location: new google.maps.LatLng(collegeWP[j].lat, collegeWP[j].long)});
-        } else {
-            mapPlot.push(new google.maps.Marker({
-                position: {lat: collegeWP[j].lat, lng: collegeWP[j].long},
-                map: map,
-                title: collegeWP[j].name
-            }));
+    var mapPlot = [];
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            console.log('got-pos');
+            latlong.push({location: new google.maps.LatLng(position.coords.latitude, position.coords.longitude)});
+            for (var j = 0; j < collegeWP.length; j++) {
+                if (routeNeeded) {
+                    latlong.push({location: new google.maps.LatLng(collegeWP[j].lat, collegeWP[j].long)});
+                } else {
+                    mapPlot.push(new google.maps.Marker({
+                        position: {lat: collegeWP[j].lat, lng: collegeWP[j].long},
+                        map: map,
+                        title: collegeWP[j].name
+                    }));
+                }
+            }
+            latlong.push({location: new google.maps.LatLng(position.coords.latitude, position.coords.longitude)});
+            if (routeNeeded) {
+                console.log('routing');
+                displayRoute(latlong[0], latlong[latlong.length - 1], map, latlong.slice(1, latlong.length - 1));
+            }
+        }, function () {
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        console.log('not-got-pos');
+        for (var j = 0; j < collegeWP.length; j++) {
+            if (routeNeeded) {
+                latlong.push({location: new google.maps.LatLng(collegeWP[j].lat, collegeWP[j].long)});
+            } else {
+                mapPlot.push(new google.maps.Marker({
+                    position: {lat: collegeWP[j].lat, lng: collegeWP[j].long},
+                    map: map,
+                    title: collegeWP[j].name
+                }));
+            }
         }
-    }
-    if (routeNeeded) {
-        displayRoute(latlong[0], latlong[latlong.length - 1], map, latlong.slice(1, latlong.length - 1));
+        if (routeNeeded) {
+            console.log('routing');
+            displayRoute(latlong[0], latlong[latlong.length - 1], map, latlong.slice(1, latlong.length - 1));
+        }
+
     }
 
 };
