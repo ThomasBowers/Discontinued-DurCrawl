af9fd6a12b775b36743724810df0daaeb89d034f
thomasbowers <thomas.bowers@durham.ac.uk>
2019-03-08 17:28:59 +0000

    Added compression

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require("body-parser");
-
+const compression = require('compression');
 
 var indexRouter = require('./routes/index');
 var aboutRouter = require('./routes/about');
@@ -16,7 +16,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+//compression
+app.use(compression({level: 9}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
